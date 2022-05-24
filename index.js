789ad09c22f2a8126718a0e914ee828ab5235e3e
Joseph Hirotsu <jhirotsu@gmail.com>
2022-05-24 20:33:04 +0900

    commit tree 79131e90e90b4fc9ca694cfe558434e4d8bcb532

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
-}
+
+    const input = document.getElementById('button');
+    input.addingEventListener('click', function() {
+      alert('I was clicked!');
+    });
+addingEventListener();
